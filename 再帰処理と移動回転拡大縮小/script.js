0589cf46e1a5034b54483e0fdb54c3478648ea30
yukirin <standupdown@gmail.com>
2018-11-27 23:46:09 +0900

    Update

--- 再帰処理と移動回転拡大縮小/script.js
+++ 再帰処理と移動回転拡大縮小/script.js
@@ -8,11 +8,10 @@ onload = function () {
 
 function renderWebGL() {
 	var c = document.getElementById('canvas');
-	c.width = 300;
+	c.width = 500;
 	c.height = 300;
 
 	gl = c.getContext('webgl') || c.getContext('experimental-webgl');
-	clearBuffer([0.0, 0.0, 0.0, 1.0], 1.0);
 
 	var prg = create_program('vertex.glsl', 'x-vertex', 'fragment.glsl', 'x-fragment');
 
@@ -37,22 +36,43 @@ function renderWebGL() {
 	var tmpMatrix = m.identity(m.create());
 	var mvpMatrix = m.identity(m.create());
 
-	m.lookAt([0.0, 0.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
-	m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
+	m.lookAt([0.0, 0.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+	m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
 	m.multiply(pMatrix, vMatrix, tmpMatrix);
 
-	m.translate(mMatrix, [1.5, 0.0, 0.0], mMatrix);
-	m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+	var count = 0;
 
-	gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	(function () {
+		clearBuffer([0.0, 0.0, 0.0, 1.0], 1.0);
+		count++
 
-	m.identity(mMatrix);
-	m.translate(mMatrix, [-1.5, 0.0, 0.0], mMatrix);
-	m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+		var rad = (count % 360) * Math.PI / 180;
+		var x = Math.cos(rad);
+		var y = Math.sin(rad);
 
-	gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+		m.identity(mMatrix);
+		m.translate(mMatrix, [x, y + 1.0, 0.0], mMatrix);
+		m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+		gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+		gl.drawArrays(gl.TRIANGLES, 0, 3);
 
-	gl.flush();
+		m.identity(mMatrix);
+		m.translate(mMatrix, [1.0, -1.0, 0.0], mMatrix);
+		m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+		m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+		gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+		gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+		var s = Math.sin(rad) + 1.0;
+		m.identity(mMatrix);
+		m.translate(mMatrix, [-1.0, -1.0, 0.0], mMatrix);
+		m.scale(mMatrix, [s, s, 0.0], mMatrix);
+		m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+		gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+		gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+		gl.flush();
+
+		setTimeout(arguments.callee, 1000 / 30);
+	})();
 }
