86f2b7c9fc42059c2cc9f79031c7e59390e94d8f
yukirin <standupdown@gmail.com>
2018-11-29 20:51:20 +0900

    Add 環境光によるライティング

---
+++ 環境光によるライティング/vertex.glsl
@@ -0,0 +1,19 @@
+
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec4 color;
+
+uniform mat4 mvpMatrix;
+uniform mat4 invMatrix;
+uniform vec3 lightDirection;
+uniform vec4 ambientColor;
+
+varying vec4 vColor;
+
+void main(void) {
+    vec3 invLight = normalize(invMatrix * vec4(lightDirection, 0.0)).xyz;
+    float diffuse = clamp(dot(normal, invLight), 0.0, 1.0);
+
+    vColor = color * vec4(vec3(diffuse), 1.0) + ambientColor;
+    gl_Position = mvpMatrix * vec4(position, 1.0);
+}
