17b46b0072112e4591addfc575daa1795b7b6811
yukirin <standupdown@gmail.com>
2018-12-17 20:01:35 +0900

    Update template

--- template/initgl2.js
+++ template/initgl2.js
@@ -149,9 +149,13 @@ function create_texture(source, number) {
         gl.bindTexture(gl.TEXTURE_2D, tex);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
         gl.generateMipmap(gl.TEXTURE_2D);
-        gl.bindTexture(gl.TEXTURE_2D, null);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
         textures[number] = tex;
+        gl.bindTexture(gl.TEXTURE_2D, null);
     };
 
     img.src = source;
