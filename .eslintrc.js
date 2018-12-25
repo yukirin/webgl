9d83b7b799ee2c09e7f996c7165a7ffac35834b3
yukirin <standupdown@gmail.com>
2018-12-26 05:46:26 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "google"
+  'extends': 'google',
+  'parserOptions': {'ecmaVersion': 2017},
+  'rules': {
+    'linebreak-style': ['error', 'windows'],
+    'require-jsdoc': 0,
+    'max-len': ['error', {'code': 120}],
+    'no-unused-vars': [0],
+    'new-cap': [0]
+  }
 };
