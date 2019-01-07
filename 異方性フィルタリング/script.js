a631132be20a665bf2b9c3de52d7a8e2931adfe1
yukirin <standupdown@gmail.com>
2019-01-07 16:03:28 +0900

    Add 異方性フィルタリング

---
+++ 異方性フィルタリング/script.js
@@ -0,0 +1,123 @@
+let c, gl, ext;
+let then = 0;
+let cubeTexture = null;
+const m = new matIV();
+const q = new qtnIV();
+const textures = [];
+const cameraQt = q.identity(q.create());
+
+onload = function() {
+  const eRange = document.getElementById('range');
+  const eCheck = document.getElementById('checkbox');
+
+  c = document.getElementById('canvas');
+  c.width = 1280;
+  c.height = 720;
+  c.addEventListener('mousemove', mouseMove, true);
+
+  gl = c.getContext('webgl', {stencil: true}) || c.getContext('experimental-webgl', {stencil: true});
+  ext = getExtensions(gl);
+  const prg = create_program('lighting.vert', 'x-vertex', 'lighting.frag', 'x-fragment');
+  create_texture('texture.png', 0);
+
+  const fBufferSize = 2048;
+
+  const pData = plane();
+  const vaos = {};
+  vaos.plane = createVAO(
+      [pData.p, pData.c, pData.n, pData.t], ['position', 'color', 'normal', 'texCoord'], [3, 4, 3, 2], pData.i, prg);
+
+  const [mMatrix, vMatrix, pMatrix, tmpMatrix, mvpMatrix, invTMatrix, invMatrix, tMatrix] = initialMatrix(8);
+  const lightDirection = [1.0, 1.0, 2.0];
+  const initCamPosition = [0.0, 0.2, 1.0];
+  const initCamUpDirection = [0, 1, 0];
+  const camPosition = [];
+  const camUpDirection = [];
+  const ambientColor = [0.10, 0.10, 0.10, 1.0];
+  let count = 0;
+
+  gl.depthFunc(gl.LEQUAL);
+  gl.enable(gl.DEPTH_TEST);
+  gl.frontFace(gl.CCW);
+  gl.cullFace(gl.BACK);
+  gl.enable(gl.CULL_FACE);
+  // gl.enable(gl.STENCIL_TEST);
+  // gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+  // gl.blendEquationSeparate(gl.FUNC_ADD, gl.FUNC_ADD);
+  // gl.enable(gl.BLEND);
+
+  function render(scale, rad, axis, translate, textures, indexSize, linkValues, linkNames, linkTypes, prg) {
+    bind_texture(textures);
+
+    m.identity(mMatrix);
+    m.translate(mMatrix, translate, mMatrix);
+    m.rotate(mMatrix, rad, axis, mMatrix);
+    m.scale(mMatrix, scale, mMatrix);
+
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+    m.inverse(mMatrix, invMatrix);
+    m.transpose(invMatrix, invTMatrix);
+
+    linkUniform(linkValues, linkNames, linkTypes, prg);
+    gl.drawElements(gl.TRIANGLES, indexSize, gl.UNSIGNED_SHORT, 0);
+  }
+
+  function setVPMatrix(fovy, width, height, near, far, isOrtho = false) {
+    const center = [0.0, 0.0, 0.0];
+    q.toVecIII(initCamPosition, cameraQt, camPosition);
+    q.toVecIII(initCamUpDirection, cameraQt, camUpDirection);
+
+    if (isOrtho) {
+      m.lookAt([0.0, 0.0, 0.5], center, [0, 1, 0], vMatrix);
+      m.ortho(-1.0, 1.0, 1.0, -1.0, 0.1, 100, pMatrix);
+    } else {
+      m.lookAt(camPosition, center, camUpDirection, vMatrix);
+      m.perspective(fovy, width / height, near, far, pMatrix);
+    }
+    m.multiply(pMatrix, vMatrix, tmpMatrix);
+  }
+
+
+  function renderFrame(now) {
+    now *= 0.001;
+    const deltaTime = now - then;
+    then = now;
+    const state = eCheck.checked;
+    const strength = eRange.value;
+
+    count++;
+    const rad = (count % 720) * Math.PI / 360;
+    const hsv = hsva((count / 2) % 360, 1, 1, 1);
+
+    changePrgFramebuffer(prg, null, [c.width, c.height]);
+    setVPMatrix(45, c.width, c.height, 0.1, 150);
+    ext.vao.bindVertexArrayOES(vaos.plane);
+    gl.viewport(0.0, 0.0, c.width, c.height / 2);
+    gl.bindTexture(gl.TEXTURE_2D, textures[0]);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, ext.aft.TEXTURE_MAX_ANISOTROPY_EXT, 16);
+    render(
+        [1.0, 1.0, 1.0], 0, [1, 1, 0], [0, 0, 0], [textures[0]], pData.i.length,
+        [mMatrix, mvpMatrix, invTMatrix, lightDirection, camPosition, ambientColor, 0],
+        ['mMatrix', 'mvpMatrix', 'invTMatrix', 'lightDirection', 'eyePosition', 'ambientColor', 'texture'],
+        ['m4', 'm4', 'm4', 'v3', 'v3', 'v4', 'i1'], prg);
+
+    gl.viewport(0.0, c.height / 2, c.width, c.height / 2);
+    gl.bindTexture(gl.TEXTURE_2D, textures[0]);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, ext.aft.TEXTURE_MAX_ANISOTROPY_EXT, 1);
+    render(
+        [1.0, 1.0, 1.0], 0, [1, 1, 0], [0, 0, 0], [textures[0]], pData.i.length,
+        [mMatrix, mvpMatrix, invTMatrix, lightDirection, camPosition, ambientColor, 0],
+        ['mMatrix', 'mvpMatrix', 'invTMatrix', 'lightDirection', 'eyePosition', 'ambientColor', 'texture'],
+        ['m4', 'm4', 'm4', 'v3', 'v3', 'v4', 'i1'], prg);
+
+    gl.flush();
+    requestAnimationFrame(renderFrame);
+  };
+
+  requestAnimationFrame(renderFrame);
+};
