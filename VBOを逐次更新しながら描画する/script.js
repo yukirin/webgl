48100b16c2e1d73f79adc6bac862df9bdda9cdf4
yukirin <standupdown@gmail.com>
2019-01-08 17:29:46 +0900

    Add VBOを逐次更新しながら描画する

---
+++ VBOを逐次更新しながら描画する/script.js
@@ -0,0 +1,135 @@
+let c, gl, ext;
+let then = 0;
+let cubeTexture = null;
+const m = new matIV();
+const q = new qtnIV();
+const textures = [];
+const cameraQt = q.identity(q.create());
+
+onload = function() {
+  const eRange = document.getElementById('range');
+  const eCheck = document.getElementById('checkbox');
+
+  c = document.getElementById('canvas');
+  c.width = 1280;
+  c.height = 720;
+  c.addEventListener('mousemove', mouseMove, true);
+
+  gl = c.getContext('webgl', {stencil: true}) || c.getContext('experimental-webgl', {stencil: true});
+  ext = getExtensions(gl);
+  const prg = create_program('lighting.vert', 'x-vertex', 'lighting.frag', 'x-fragment');
+  const fBufferSize = 2048;
+
+  const spData = sphere(64, 64, 1.5);
+  const vaos = {};
+  vaos.sphere = createVAO([spData.c], ['color'], [4], [0], spData.i, prg);
+  const pPosition = new Float32Array(spData.p);
+
+  ext.vao.bindVertexArrayOES(vaos.sphere);
+  const vbo = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bufferData(gl.ARRAY_BUFFER, pPosition, gl.DYNAMIC_DRAW);
+  let attLocation = gl.getAttribLocation(prg, 'position');
+  gl.enableVertexAttribArray(attLocation);
+  gl.vertexAttribPointer(attLocation, 3, gl.FLOAT, false, 0, 0);
+  ext.vao.bindVertexArrayOES(null);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  const [mMatrix, vMatrix, pMatrix, tmpMatrix, mvpMatrix, invTMatrix, invMatrix, tMatrix] = initialMatrix(8);
+  const lightDirection = [1.0, 1.0, 2.0];
+  const initCamPosition = [0.0, 5.0, 6.0];
+  const initCamUpDirection = [0, 1, 0];
+  const camPosition = [];
+  const camUpDirection = [];
+  const ambientColor = [0.10, 0.10, 0.10, 1.0];
+  let count = 0;
+
+  gl.depthFunc(gl.LEQUAL);
+  gl.enable(gl.DEPTH_TEST);
+  gl.frontFace(gl.CCW);
+  gl.cullFace(gl.BACK);
+  gl.enable(gl.CULL_FACE);
+  // gl.enable(gl.STENCIL_TEST);
+  // gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+  // gl.blendEquationSeparate(gl.FUNC_ADD, gl.FUNC_ADD);
+  // gl.enable(gl.BLEND);
+
+  function render(
+      scale, rad, axis, translate, textures, indexSize, linkValues, linkNames, linkTypes, prg, isDrawPoints = false,
+      isInstanced = false, insCount = 1) {
+    bind_texture(textures);
+
+    m.identity(mMatrix);
+    m.translate(mMatrix, translate, mMatrix);
+    m.rotate(mMatrix, rad, axis, mMatrix);
+    m.scale(mMatrix, scale, mMatrix);
+
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+    m.inverse(mMatrix, invMatrix);
+    m.transpose(invMatrix, invTMatrix);
+
+    linkUniform(linkValues, linkNames, linkTypes, prg);
+
+    if (isInstanced) {
+      ext.ins.drawElementsInstancedANGLE(gl.TRIANGLES, indexSize, gl.UNSIGNED_SHORT, 0, insCount);
+      return;
+    }
+
+    if (isDrawPoints) {
+      gl.drawArrays(gl.POINTS, 0, indexSize);
+      return;
+    }
+
+    gl.drawElements(gl.TRIANGLES, indexSize, gl.UNSIGNED_SHORT, 0);
+  }
+
+  function setVPMatrix(fovy, width, height, near, far, isOrtho = false) {
+    const center = [0.0, 0.0, 0.0];
+    q.toVecIII(initCamPosition, cameraQt, camPosition);
+    q.toVecIII(initCamUpDirection, cameraQt, camUpDirection);
+
+    if (isOrtho) {
+      m.lookAt([0.0, 0.0, 0.5], center, [0, 1, 0], vMatrix);
+      m.ortho(-1.0, 1.0, 1.0, -1.0, 0.1, 100, pMatrix);
+    } else {
+      m.lookAt(camPosition, center, camUpDirection, vMatrix);
+      m.perspective(fovy, width / height, near, far, pMatrix);
+    }
+    m.multiply(pMatrix, vMatrix, tmpMatrix);
+  }
+
+
+  function renderFrame(now) {
+    now *= 0.001;
+    const deltaTime = now - then;
+    then = now;
+    const state = eCheck.checked;
+    const difference = eRange.value / 100;
+
+    count++;
+    const rad = (count % 720) * Math.PI / 360;
+    const hsv = hsva((count / 2) % 360, 1, 1, 1);
+
+    for (let i = 0, j = pPosition.length; i < j; i += 3) {
+      const t = Math.cos(rad * 2);
+      pPosition[i] = spData.p[i] + spData.p[i] * t;
+      pPosition[i + 1] = spData.p[i + 1] + spData.p[i + 1] * t;
+      pPosition[i + 2] = spData.p[i + 2] + spData.p[i + 2] * t;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, pPosition);
+
+    changePrgFramebuffer(prg, null, [c.width, c.height]);
+    setVPMatrix(45, c.width, c.height, 0.1, 150);
+    ext.vao.bindVertexArrayOES(vaos.sphere);
+    render(
+        [1.0, 1.0, 1.0], rad, [1, 1, 1], [0, 0, 0], [], spData.p.length / 3, [mvpMatrix], ['mvpMatrix'], ['m4'], prg,
+        true);
+
+    gl.flush();
+    requestAnimationFrame(renderFrame);
+  };
+
+  requestAnimationFrame(renderFrame);
+};
